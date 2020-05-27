0ab7d7d21e0af0e01f337e7fb82f39f96fdc303e
vhaiswkavurt <vhaiswkavurt@VACOLTKAVURT1.dva.va.gov>
2020-05-27 14:33:48 -0400

    added CORS support

--- src/main/java/gov/va/med/es/fhir/ratingservice/RatingserviceApplication.java
+++ src/main/java/gov/va/med/es/fhir/ratingservice/RatingserviceApplication.java
@@ -2,11 +2,13 @@ package gov.va.med.es.fhir.ratingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@EnableAdminServer
+//CrossOrigin(origins = "http://localhost:8000", maxAge = 3600, methods = RequestMethod.DELETE, )
 @SpringBootApplication
 @ComponentScan(basePackages = {"gov.va.med.es.fhir.ratingservice", "gov.va.med.es.fhir.ratingservice.config",
 		"gov.va.med.es.fhir.ratingservice.controller"})
@@ -15,4 +17,19 @@ public class RatingserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RatingserviceApplication.class, args);
 	}
+	
+	
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/rating-service").allowedOrigins("http://localhost:8000")
+				.allowedMethods("GET", "OPTIONS");
+			}
+		};
+	}
+	
+	
 }
